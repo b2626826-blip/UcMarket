a60040ca724f6e9d4dac98f72435620b111e2ed9
eagle <b2626826@gmail.com>
2026-05-21 15:00:02 +0800

    Initial UcMarket project scaffold

---
+++ backend/src/main/java/com/ucmarket/UcMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ucmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcMarketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ucmarket/UcMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
